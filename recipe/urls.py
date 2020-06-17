8b2849a0ca638660ca938c8f8443fd2863d83c23
Tony Whittam <tony.whittam@gmail.com>
2020-06-17 17:19:16 +0100

    user accounts

--- recipe/urls.py
+++ recipe/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/register", views.register, name="register"),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -5,7 +5,22 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from datetime import datetime
 
-from .models import Ingredient, Recipe, IngredientUse, Brew, LogEntry, Image
+from .models import Ingredient, Recipe, IngredientUse, Brew, LogEntry, Image, WineStyle
 
 def index(request):
     return "HomeWineX says Hello!"
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return HttpResponseRedirect("/")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/register.html", {'form': form})
